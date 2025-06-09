3e921f2f47df34cd6a7808cb966e1ce0add3f251
Gabi Thaler <gmth054@gmail.com>
2025-06-09 17:03:20 +0300

    added the models an check connect

---
+++ Malshinon/DataBase/MySql.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Malshinon.DataBase
+{
+    internal class MySqlData
+    {
+        static string connictionString = "server=localhost;DataBase=project1;user=root;Password='';Port=3306;";
+        private MySqlConnection connection;
+
+        public void connect()
+        {
+            var conn = new MySqlConnection(connictionString);
+            connection = conn;
+
+            try
+            {
+                conn.Open();
+                Console.WriteLine("conneted to MySql database successfully.");
+                conn.Close();
+
+            }
+            catch(MySqlException ex)
+            {
+                Console.WriteLine($"Error conacting to DATA BASE: {ex.Message}");
+            }
+        }
+    }
+}
--- Malshinon/Program.cs
+++ Malshinon/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
-
+using  Malshinon.mddel;
+using  Malshinon.DataBase;
 
 namespace Malshinon;
 class program
 {
     static void Main(string[] arcs)
     {
-
+        MySqlData ms = new MySqlData();
+        ms.connect();
     }
 }
